1ab92e39e3a0fdc253af833e97a42d501b5faf08
solchanrefqialhabib <solchanrefqialhabib1gmail.com>
2025-09-19 19:09:31 +0700

    kdf

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { motion, AnimatePresence } from "framer-motion";
 
-const slideImages = ["/coffee.jpg", "/MIE.JPG", "/team.JPG"];
+const slideImages = ["/coffee.jpg", "/cili.jpg", "/team.jpg"];
 
 const Hero = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
